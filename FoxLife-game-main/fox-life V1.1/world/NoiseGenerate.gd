1d8790c91194762dc1b4cd1cf9626d41f73210d5
AnCar <87574028+AnCarsenat@users.noreply.github.com>
2023-12-02 12:14:49 +0100

    Add files via upload

--- FoxLife-game-main/fox-life V1.1/world/NoiseGenerate.gd
+++ FoxLife-game-main/fox-life V1.1/world/NoiseGenerate.gd
@@ -1,16 +1,7 @@
 extends TileMap
 #var tilemap = self.get_parent().get_node("TileMap")
-
-const image_size := Vector2(512, 512)
-const image_format := Image.FORMAT_RGB8
-const noise_types: Array = [
-	FastNoiseLite.TYPE_SIMPLEX,
-	FastNoiseLite.TYPE_SIMPLEX_SMOOTH,
-	FastNoiseLite.TYPE_CELLULAR,
-	FastNoiseLite.TYPE_PERLIN,
-	FastNoiseLite.TYPE_VALUE,
-	FastNoiseLite.TYPE_VALUE_CUBIC
-]
+var width = 4000
+var height = 50
 
 var noise: = FastNoiseLite.new()
 
@@ -20,12 +11,21 @@ func _ready():
 	noise.seed = randi()
 	noise.noise_type = FastNoiseLite.TYPE_PERLIN
 	noise.fractal_octaves = 1
-	noise.fractal_gain = 1
+	noise.fractal_gain = 0.75
+	
+	for x in range(width):
+		var noise_level = abs((noise.get_noise_2d(x,height)*50)+240)
+		var tile_Pos = local_to_map(Vector2(x,noise_level))
+		set_cell(0, Vector2i(tile_Pos.x,tile_Pos.y),0,Vector2i(1,2))
+		for y in range(1,20):
+			var dirt_Pos = local_to_map(Vector2(x,noise_level+10*y))
+			set_cell(0, Vector2i(dirt_Pos.x,dirt_Pos.y),0,Vector2i(1,3))
 
 func _process(delta):
-	for x in range(0, image_size.x):
-		var noise_level = (noise.get_noise_1d(x)*5)+240
-		set_cell(1,Vector2i(x,noise_level),-1)
+	pass
+			
+			
+		
 		
 		
 			
