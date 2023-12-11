551b4774af930c97e438112f1e83bbddb1cd5a2e
AnCar <87574028+AnCarsenat@users.noreply.github.com>
2023-12-11 13:22:09 +0100

    Add files via upload

--- FoxLife-game-main/fox-life V1.1/world/NoiseGenerate.gd
+++ FoxLife-game-main/fox-life V1.1/world/NoiseGenerate.gd
@@ -7,18 +7,18 @@ var noise: = FastNoiseLite.new()
 
 func _ready():
 	randomize()
-	
 	noise.seed = randi()
 	noise.noise_type = FastNoiseLite.TYPE_PERLIN
 	noise.fractal_octaves = 1
 	noise.fractal_gain = 0.75
-	
 	for x in range(width):
-		var noise_level = abs((noise.get_noise_2d(x,height)*50)+240)
+		var noise_level = abs((noise.get_noise_2d(x,height)*150)+240)
+		if(noise_level < 0): 
+			height = 0
 		var tile_Pos = local_to_map(Vector2(x,noise_level))
 		set_cell(0, Vector2i(tile_Pos.x,tile_Pos.y),0,Vector2i(1,2))
 		for y in range(1,20):
-			var dirt_Pos = local_to_map(Vector2(x,noise_level+10*y))
+			var dirt_Pos = local_to_map(Vector2(x,noise_level+15*y))
 			set_cell(0, Vector2i(dirt_Pos.x,dirt_Pos.y),0,Vector2i(1,3))
 
 func _process(delta):
